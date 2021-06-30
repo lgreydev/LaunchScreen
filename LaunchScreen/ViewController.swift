4311b4c46bc506f022efcc392e118faab9117e1f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-30 11:30:37 +0300

    Initial Commit

---
+++ LaunchScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchScreen
+//
+//  Created by Sergey Lukaschuk on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
